7aff42d2e8c671b866b3b5484ad44ae0603ca635
Reid Williams <reidbeats@gmail.com>
2023-02-25 01:50:52 -0600

    changing assert function and adding gitignore for pycache

--- src/split_files.py
+++ src/split_files.py
@@ -8,11 +8,16 @@ FIELDS = (
     'Series',
 )
 
+def assert_file_name_list(file_name_list):
+    file_name_list_length = len(file_name_list)
+    field_length = len(FIELDS)
+    assert file_name_list_length >= 1, "File must have a name"
+    assert file_name_list_length <= field_length, f"File can only have {field_length} has {file_name_list_length}"
+
 def file_name_to_dict(file):
     renamed_file = str(file).split('.jpg')[0]
     splitted_file = renamed_file.split('_')
-    assert len(splitted_file) >= 1
-    assert len(splitted_file) <= len(FIELDS)
+    assert_file_name_list(splitted_file)
     output = {}
     for idx, field in enumerate(FIELDS):
         try:
