b5b3a4006810d50a04644de0722244ea21bc7717
reidwil <44632147+reidwil@users.noreply.github.com>
2023-02-27 09:13:07 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,21 +2,8 @@ from pathlib import Path
 from split_files import file_name_to_dict, convert_dicts_to_csv
 
 
-def list_files(location="./Photos/2022-01"):
+def list_files(location="./Photos/"):
     return list(Path(location).rglob("*.jpg"))
-
-
-def test_list_files():
-    return [
-        "Mad_EC_1_%35_July_1958.jpg",
-        "Detective Comics_DC_1_1_Sep_1940.jpg",
-        "Mad___110_Sep_1966.jpg",
-        "Mad_ _ _110_Sep_1966.jpg",
-        "Mad_     _     _110_Sep_1966.jpg",
-        "Detective Comics___4__1953.jpg",
-        "Mad___250.jpg",
-        "Simpsons Shenanigans_Bongo.jpg",
-    ]
 
 
 def sort_file_names_to_dict(files: list):
